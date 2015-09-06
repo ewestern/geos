f6cf7470b788e70a7f346e6e04a1f93e2d4f3bb2
= <=>
2015-09-06 13:51:06 -0400

    builds on darwin

---
+++ README.md
@@ -0,0 +1,11 @@
+## GEOS
+This is a Haskell binding to Geos, the open-source geometry library. Capabilities include:
+
+* Geometries: Point, LineString, Polygon, MultiPoint, MultiLineString, MultiPolygon, GeometryCollection
+* Predicates: Intersects, Touches, Disjoint, Crosses, Within, Contains, Overlaps, Equals, Covers
+* Operations: Union, Distance, Intersection, Symmetric Difference, Convex Hull, Envelope, Buffer, Simplify, Polygon Assembly, Valid, Area, Length,
+* Prepared geometries (pre-spatially indexed)
+* STR spatial index
+* OGC Well Known Text (WKT) and Well Known Binary (WKB) encoders and decoders. These can be used to parse PostGIS geometry types.
+
+
--- Setup.hs
+++ Setup.hs
@@ -6,40 +6,6 @@ import Distribution.PackageDescription
 import Distribution.Version
 
 import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Program
-import Distribution.Verbosity
+import Distribution.Simple
+main = defaultMain
 
-import Data.Char (isSpace)
-import Data.List (dropWhile,reverse, stripPrefix)
-import Data.Maybe (fromJust)
-
-main = putStrLn "Run Setup" >> defaultMainWithHooks simpleUserHooks {
-  confHook = \pkg flags -> do
-  if lookup (FlagName "use-pkg-config") 
-            (configConfigurationsFlags flags) == Just True
-  then do
-    confHook simpleUserHooks pkg flags
-  else do
-    lbi <- confHook simpleUserHooks pkg flags
-    bi <- geosBuildInfo lbi
-
-    return lbi {
-      localPkgDescr = updatePackageDescription
-                        (Just bi, [("runtests", bi)]) (localPkgDescr lbi)
-    }
-}
-
-geosBuildInfo :: LocalBuildInfo -> IO BuildInfo
-geosBuildInfo lbi = do
-  (pgconfigProg, _) <- requireProgram verbosity
-                           (simpleProgram "geos-config") (withPrograms lbi)
-  let geosconfig = rawSystemProgramStdout verbosity pgconfigProg
-  incDir <- geosconfig ["--includes"]
-  libDir <- geosconfig ["--ldflags"]
-  return emptyBuildInfo {
-    extraLibDirs = [strip . fromJust $ stripPrefix "-L" libDir],
-    includeDirs  = [strip incDir]
-  }
-  where
-    verbosity = normal -- honestly, this is a hack
-    strip x = dropWhile isSpace $ reverse $ dropWhile isSpace $ reverse x
