a1e15c7cf4c7916ce557aee53e2c8b2411bbbf80
= <=>
2015-09-08 19:23:14 -0400

    finish buffer

--- src/GEOS/Buffer.hs
+++ src/GEOS/Buffer.hs
@@ -1,14 +1,12 @@
 module GEOS.Buffer (
     buffer
   , defaultBufferParams
-
-
+  , bufferWithParams
 ) where
 import qualified GEOS.Raw.Buffer as R
-import qualified GEOS.Raw.Geometry as RG
-import GEOS.Geometry
 import GEOS.Raw.Base
 import GEOS.Types
+import GEOS.Geometry
 
 data BufferCapStyle = 
     RoundCap
@@ -20,6 +18,18 @@ data BufferJoinStyle =
   | MitreJoin
   | BevelJoin   
 
+convertCapStyle :: BufferCapStyle -> R.BufferCapStyle
+convertCapStyle = \case
+  RoundCap -> R.capRound
+  SquareCap -> R.capSquare
+  FlatCap -> R.capFlat
+
+convertJoinStyle :: BufferJoinStyle -> R.BufferJoinStyle
+convertJoinStyle = \case
+  RoundJoin -> R.joinRound
+  MitreJoin -> R.joinMitre
+  BevelJoin -> R.joinBevel
+
 data BufferParams = BufferParams {
     joinStyle :: BufferJoinStyle
   , capStyle :: BufferCapStyle 
@@ -30,6 +40,7 @@ data BufferParams = BufferParams {
   , singleSided :: Bool
 }
 
+defaultBufferParams :: BufferParams
 defaultBufferParams = BufferParams {
     joinStyle = RoundJoin
   , capStyle = RoundCap
@@ -44,3 +55,14 @@ buffer g width quadsegs = runGeos $  do
     rg' <- R.buffer rg width quadsegs
     convertGeometryFromRaw rg'
 
+bufferWithParams :: Geometry -> Double -> BufferParams -> Geometry
+bufferWithParams g width bp = runGeos $ do
+  rg <- convertGeometryToRaw g
+  rbp <- R.createBufferParams 
+  R.setEndCapStyle rbp (convertCapStyle $ capStyle bp) 
+  R.setJoinStyle rbp (convertJoinStyle $ joinStyle bp)
+  R.setMitreLimit rbp (mitreLimit bp)
+  R.setQuadrantSegments rbp (quadrantSegments bp)
+  R.setSingleSided rbp (singleSided bp)
+  rg' <- R.bufferWithParams rg rbp width  
+  convertGeometryFromRaw rg'
--- src/GEOS/Raw/Buffer.hs
+++ src/GEOS/Raw/Buffer.hs
@@ -1,6 +1,8 @@
 module GEOS.Raw.Buffer (
     BufferParams
   , withBufferParams
+  , BufferCapStyle
+  , BufferJoinStyle
   , buffer
   , createBufferParams
   , setEndCapStyle
@@ -20,15 +22,8 @@ module GEOS.Raw.Buffer (
 )  where
 import GEOS.Raw.Internal 
 import qualified GEOS.Raw.Geometry as RG
-import GEOS.Geometry
 import GEOS.Raw.Base
-import GEOS.Raw.CoordSeq
 import Foreign
-import Foreign.C.Types
-import Foreign.ForeignPtr
-import Foreign.Marshal.Utils
-import Data.Monoid ((<>))
-import qualified Data.Vector as V
 
 newtype BufferParams = BufferParams {
   unBufferParams :: ForeignPtr GEOSBufferParams 
@@ -57,56 +52,54 @@ createBufferParams = withGeos $ \h -> do
 
 setEndCapStyle :: BufferParams -> BufferCapStyle -> Geos ()
 setEndCapStyle b s = withGeos $ \h -> do
-  throwIfZero (mkErrorMessage "setEndCapStyle") $ withBufferParams b $ \bp -> 
+  _ <- throwIfZero (mkErrorMessage "setEndCapStyle") $ withBufferParams b $ \bp -> 
       geos_BufferParamsSetEndCapStyle h bp $ unBufferCapStyle s  
   return ()
 
 setJoinStyle :: BufferParams -> BufferJoinStyle -> Geos ()
 setJoinStyle b s = withGeos $ \h -> do
-  throwIfZero (mkErrorMessage "setJoinStyle") $ withBufferParams b $ \bp -> 
+  _ <- throwIfZero (mkErrorMessage "setJoinStyle") $ withBufferParams b $ \bp -> 
       geos_BufferParamsSetJoinStyle h bp $ unBufferJoinStyle s  
   return ()
 
 
 setMitreLimit :: BufferParams -> Double -> Geos ()
 setMitreLimit b d = withGeos $ \h -> do
-  throwIfZero (mkErrorMessage "setJoinStyle") $ withBufferParams b $ \bp -> 
+  _ <- throwIfZero (mkErrorMessage "setJoinStyle") $ withBufferParams b $ \bp -> 
       geos_BufferParamsSetMitreLimit h bp $ realToFrac d  
   return ()
 
 setQuadrantSegments :: BufferParams -> Int -> Geos ()
 setQuadrantSegments b i = withGeos $ \h -> do
-  throwIfZero (mkErrorMessage "setJoinStyle") $ withBufferParams b $ \bp -> 
+  _ <- throwIfZero (mkErrorMessage "setJoinStyle") $ withBufferParams b $ \bp -> 
       geos_BufferParamsSetQuadrantSegments h bp $ fromIntegral i 
   return ()
 
 setSingleSided :: BufferParams -> Bool -> Geos ()
 setSingleSided bp b = withGeos $ \h -> do
-  throwIfZero (mkErrorMessage "setSingleSided") $ withBufferParams bp $ \bpp ->
+  _ <- throwIfZero (mkErrorMessage "setSingleSided") $ withBufferParams bp $ \bpp ->
     geos_BufferParamsSetSingleSided h bpp $ fromBool b
   return ()
 
 bufferWithParams :: RG.Geometry -> BufferParams -> Double -> Geos RG.Geometry
 bufferWithParams g b width = withGeos $ \h -> do
-  g <- throwIfNull "bufferWithParams" $ RG.withGeometry g $ \gp ->
+  g' <- throwIfNull "bufferWithParams" $ RG.withGeometry g $ \gp ->
     withBufferParams b $ \bp -> 
       geos_BufferWithParams h gp bp $ realToFrac width
-  fp <- newForeignPtrEnv geos_GeomDestroy h g
+  fp <- newForeignPtrEnv geos_GeomDestroy h g'
   return $ RG.Geometry fp
 
 bufferWithStyle :: RG.Geometry -> Double -> Int -> BufferCapStyle -> BufferJoinStyle -> Double -> Geos RG.Geometry
 bufferWithStyle g width quadsegs capstyle joinstyle mitrelimit = withGeos $ \h -> do
-  g <- throwIfNull "bufferWithStyle" $ RG.withGeometry g $ \gp ->
+  g' <- throwIfNull "bufferWithStyle" $ RG.withGeometry g $ \gp ->
         geos_BufferWithStyle h gp (realToFrac width) (fromIntegral quadsegs) (unBufferCapStyle capstyle) (unBufferJoinStyle joinstyle) $ realToFrac mitrelimit
-  fp <- newForeignPtrEnv geos_GeomDestroy h g
+  fp <- newForeignPtrEnv geos_GeomDestroy h g'
   return $ RG.Geometry fp
 
 -- | Will only accept LineString geometries. For the 'width' parameter, negative doubles represent a right-side offset, and positive doubles represent a left-side offset. 
 offsetCurve :: RG.Geometry -> Double -> Int -> BufferJoinStyle -> Double -> Geos RG.Geometry 
 offsetCurve g width quadsegs joinstyle mitrelimit = withGeos $ \h -> do
-  g <- throwIfNull "offsetCurve" $ RG.withGeometry g $ \gp ->
+  g' <- throwIfNull "offsetCurve" $ RG.withGeometry g $ \gp ->
     geos_OffsetCurve h gp (realToFrac width) (fromIntegral quadsegs) (unBufferJoinStyle joinstyle) $ realToFrac mitrelimit
-  fp <- newForeignPtrEnv geos_GeomDestroy h g
+  fp <- newForeignPtrEnv geos_GeomDestroy h g'
   return $ RG.Geometry fp
-
-
