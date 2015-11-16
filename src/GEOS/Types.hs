8bb87f5a8f0f015eda30a76d4cf4ea46687cc4bf
Peter <pfrance@gmail.com>
2015-11-16 10:31:42 -0500

    derive typeable

--- src/GEOS/Types.hs
+++ src/GEOS/Types.hs
@@ -2,6 +2,7 @@ module GEOS.Types where
 import qualified Data.Vector as V
 import Data.Monoid
 import Data.Data
+import Data.Typeable
 
 type SRID = Int
 data Geometry = 
@@ -10,7 +11,7 @@ data Geometry =
   | PolygonGeometry Polygon SRID
   | MultiPointGeometry MultiPoint SRID
   | MultiLineStringGeometry MultiLineString SRID
-  | MultiPolygonGeometry MultiPolygon SRID deriving (Show, Eq)
+  | MultiPolygonGeometry MultiPolygon SRID deriving (Show, Eq, Data, Typeable)
 
 data Coordinate =
     Coordinate2 {-# UNPACK #-} !Double {-# UNPACK #-} !Double
@@ -27,12 +28,12 @@ dimensionsCoordinateSequence = dimensionsCoordinate . V.head
 
 newtype Point = Point {
   unPoint :: Coordinate
-} deriving (Show, Eq)
+} deriving (Show, Eq, Data, Typeable)
 
 -- A LinearRing is a LineString that is closed
 newtype LinearRing = LinearRing {
   unLinearRing :: CoordinateSequence
-} deriving (Show, Eq)
+} deriving (Show, Eq, Data, Typeable)
 
 instance Monoid LinearRing where
   mempty  = LinearRing V.empty
@@ -40,7 +41,7 @@ instance Monoid LinearRing where
 
 newtype LineString = LineString {
   unLineString :: CoordinateSequence
-} deriving (Show, Eq)
+} deriving (Show, Eq, Data, Typeable)
 
 instance Monoid LineString where
   mempty  = LineString V.empty
@@ -49,11 +50,11 @@ instance Monoid LineString where
 -- | In a polygon, the fist LinearRing is the shell, and any following are holes.
 newtype Polygon = Polygon {
   unPolygon :: V.Vector LinearRing
-} deriving (Show, Eq)
+} deriving (Show, Eq, Data, Typeable)
 
 newtype MultiPoint = MultiPoint {
   unMultiPoint :: V.Vector Point
-} deriving (Show, Eq)
+} deriving (Show, Eq, Data, Typeable)
 
 instance Monoid MultiPoint where
   mempty  = MultiPoint V.empty
@@ -61,9 +62,9 @@ instance Monoid MultiPoint where
 
 newtype MultiLineString = MultiLineString {
   unMultiLineString :: V.Vector LineString
-} deriving (Show, Eq)
+} deriving (Show, Eq, Data, Typeable)
 
 newtype MultiPolygon = MultiPolygon {
   unMultiPolygon :: V.Vector Polygon
-} deriving (Show, Eq)
+} deriving (Show, Eq, Data, Typeable)
 
