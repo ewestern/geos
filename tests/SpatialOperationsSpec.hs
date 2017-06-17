e1931e0bc4006fab12ed63ef1375a3e973aa0f45
Jamie Cook <jamie.cook@veitchlister.com.au>
2017-06-18 00:47:01 +1000

    Add helper for polygons with holes and test contains for it

--- tests/SpatialOperationsSpec.hs
+++ tests/SpatialOperationsSpec.hs
@@ -11,22 +11,27 @@ import Data.Geometry.Geos.Geometry
 import SpecSampleData
 
 spatialOpsSpecs = describe "Tests Contains" $ do
-  it "Does simple polygon to point comparison" $ do
-    let polygon  = makePolygonGeo [(0,0), (0,1), (1,1), (1,0), (0,0)]
-        pointIn  = makePointGeo (0.5, 0.5)
-        pointOut = makePointGeo (1.5, 0.5)
+  it "Does polygon to point comparison" $ do
+    let polygon         = makePolygonGeo [[(0,0), (0,1), (1,1), (1,0), (0,0)]]
+        polygonWithHole = makePolygonGeo [ [(0,0), (0,1), (1,1), (1,0), (0,0)], [(0.1,0.1),(0.1,0.9),(0.9,0.1),(0.1,0.1)] ]
+        pointIn         = makePointGeo (0.6, 0.6)
+        pointOut        = makePointGeo (1.5, 0.5)
     (contains polygon pointIn) `shouldBe` True
     (contains polygon pointOut) `shouldBe` False
+    (contains polygonWithHole pointIn) `shouldBe` True
+    (contains polygonWithHole pointOut) `shouldBe` False
   it "Does simple polygon to polygon comparison" $ do
-    let polygonBig   = makePolygonGeo [(0,0), (0,2), (2,2), (2,0), (0,0)]
-        polygonSmall = makePolygonGeo [(0,0), (0,1), (1,1), (1,0), (0,0)]
-        polygonIntersect = makePolygonGeo [(0,0), (0,2.5), (1,1), (1,0), (0,0)]
+    let polygonBig   = makePolygonGeo [ [(0,0), (0,2), (2,2), (2,0), (0,0)] ]
+        polygonSmall = makePolygonGeo [ [(0,0), (0,1), (1,1), (1,0), (0,0)] ]
+        polygonIntersect = makePolygonGeo [ [(0,0), (0,2.5), (1,1), (1,0), (0,0)] ]
     (contains polygonBig polygonSmall) `shouldBe` True
     (contains polygonBig polygonBig) `shouldBe` True
     (contains polygonBig polygonIntersect) `shouldBe` False
+
   it "Does multi polygon to point comparison" $ do
-    let polygonBig   = [(0,0), (0,2), (2,2), (2,0), (0,0)]
-        polygonSmall = [(3,0), (3,1), (4,1), (4,0), (3,0)]
+    -- pendingWith "Causes double free on memory"
+    let polygonBig   = [ [(0,0), (0,2), (2,2), (2,0), (0,0)]]
+        polygonSmall = [ [(3,0), (3,1), (4,1), (4,0), (3,0)]]
         multiPoly    = makeMultiPolygonGeo [polygonBig, polygonSmall]
         pointIn      = makePointGeo (0.5, 0.5)
         pointOut     = makePointGeo (2.5, 0.5)
--- tests/SpecSampleData.hs
+++ tests/SpecSampleData.hs
@@ -9,12 +9,14 @@ import Data.Geometry.Geos.Types
 point = PointGeometry (Point $ Coordinate2 36.1 (-119.1)) (Just 4326)
 makePoint (c1, c2) = Point $ Coordinate2 c1 c2
 makePointGeo c = PointGeometry (makePoint c) Nothing
-makePolygon = Polygon . V.singleton . LinearRing . V.map (uncurry Coordinate2) . V.fromList
+makeLinearRing cs = LinearRing . V.map (uncurry Coordinate2) . V.fromList $ cs
+makeLinearRingGeo cs = LinearRingGeometry (makeLinearRing cs) Nothing
+makePolygon = Polygon . V.fromList . (fmap makeLinearRing)
 makePolygonGeo cs = PolygonGeometry (makePolygon cs) Nothing
-makeMultiPolygon polygons = MultiPolygon . V.fromList $ (makePolygon <$> polygons)
+makeMultiPolygon = MultiPolygon . V.fromList . (fmap makePolygon)
 makeMultiPolygonGeo polygons = MultiPolygonGeometry (makeMultiPolygon polygons) Nothing
-polygon1 =  makePolygon [(0, 0), (0, 1), (1, 1), (1, 0), (0, 0)]
-polygon2 = makePolygon [(0, 0), (0, 1), (1, 1), (1, 0), (0, 0)]
+polygon1 =  makePolygon [[(0, 0), (0, 1), (1, 1), (1, 0), (0, 0)]]
+polygon2 = makePolygon [[(0, 0), (0, 1), (1, 1), (1, 0), (0, 0)]]
 
 polygonBS = "0103000020E6100000010000000C00000073E92D50491A5DC024275C1ED5DE404076E933474C1A5DC02C279CD7DBDE40406EE9A178431A5DC034271059E6DE40406DE9851C431A5DC034271C7AE6DE40406CE9F7AA421A5DC03427A07DE6DE40406CE92B3B421A5DC03427E05BE6DE40406CE955E4411A5DC03427A023E6DE404066E9E9FB3B1A5DC0282718AAD8DE404062E905D4381A5DC02027E877D1DE40406CE95FF8411A5DC014274C4CC6DE40406CE9B5EC421A5DC01427C8A2C6DE404073E92D50491A5DC024275C1ED5DE4040" :: BS.ByteString
 
