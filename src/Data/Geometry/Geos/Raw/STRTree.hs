e670c2a7c5482972e0167d0f4d737001a7cdd1bf
Jamie Cook <jamie.cook@veitchlister.com.au>
2017-06-28 23:46:26 +1000

    Holy shit I just got STRTree to work!!!

--- src/Data/Geometry/Geos/Raw/STRTree.hs
+++ src/Data/Geometry/Geos/Raw/STRTree.hs
@@ -12,15 +12,15 @@ import qualified Data.Vector as V
 --A query-only R-tree created using the Sort-Tile-Recursive (STR) algorithm. For two-dimensional spatial data.
 
 --The STR packed R-tree is simple to implement and maximizes space utilization; that is, as many leaves as possible are filled to capacity. Overlap between nodes is far less than in a basic R-tree. However, once the tree has been built (explicitly or on the first call to query), items may not be added or removed.
-newtype STRTree a = STRTree (ForeignPtr I.Data.Geometry.GeosSTRTree)
+newtype STRTree a = STRTree (ForeignPtr I.GEOSSTRTree)
   deriving (Show, Eq)
 
-withSTRTree :: STRTree a -> (Ptr I.Data.Geometry.GeosSTRTree -> IO b ) -> IO b
+withSTRTree :: STRTree a -> (Ptr I.GEOSSTRTree -> IO b ) -> IO b
 withSTRTree (STRTree t) f = withForeignPtr t f
 
 --if Data.Geometry.Geos_VERSION_MAJOR >= 3 && Data.Geometry.Geos_VERSION_MINOR > 4
 foreign import ccall "wrapper"
-  wrap :: (Ptr () -> Ptr () -> IO ()) -> IO (I.Data.Geometry.GeosQueryCallback)
+  wrap :: (Ptr () -> Ptr () -> IO ()) -> IO (I.GEOSQueryCallback)
 
 createSTRTree :: Int -> Geos (STRTree a)
 createSTRTree n = withGeos $ \h -> do
@@ -29,11 +29,11 @@ createSTRTree n = withGeos $ \h -> do
    return $ STRTree fp
 
 
-insert :: Storable b => STRTree b -> RG.Geometry -> b -> Geos (STRTree b)
-insert t g i =  
+insert :: (Storable b, RG.Geometry a)  => STRTree b -> a -> b -> Geos (STRTree b)
+insert t g i =
   withGeos $ \h -> alloca $ \ptr -> do
     poke ptr i
-    _ <- withSTRTree t $ \st -> RG.withGeometry g $ \gr -> 
+    _ <- withSTRTree t $ \st -> RG.withGeometry g $ \gr ->
         I.geos_STRTreeInsert h st gr (castPtr ptr)
     return t
 
@@ -48,15 +48,14 @@ insert t g i =
     {-readIORef r-}
 
 
-query :: Storable b => (STRTree b) -> RG.Geometry -> Geos (V.Vector b)
-query t g  = 
+query :: (Storable b, RG.Geometry a) => (STRTree b) -> a -> Geos (V.Vector b)
+query t g  =
   withGeos $ \h -> do
     r <- newIORef $ V.empty
     callback <- wrap $ \a _ -> do
       i <- peek $ castPtr a
       modifyIORef' r (\v -> V.snoc v i)
-    _ <- RG.withGeometry g $ \rg ->  
+    _ <- RG.withGeometry g $ \rg ->
           withSTRTree t $ \st -> I.geos_STRTreeQuery h st rg callback nullPtr
     readIORef r
---endif   
-
+--endif
--- src/Data/Geometry/Geos/STRTree.hs
+++ src/Data/Geometry/Geos/STRTree.hs
@@ -1,7 +1,28 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.Geometry.Geos.STRTree where
+
 import qualified Data.Geometry.Geos.Raw.STRTree as RT
+import qualified Data.Geometry.Geos.Raw.Geometry as RG
+import Control.Monad
 import Data.Geometry.Geos.Types
+import Data.Geometry.Geos.Geometry
+import Data.Geometry.Geos.Raw.Base
+import Foreign
+import qualified Data.Vector as V
 
+createSTR :: Storable b => V.Vector (Geometry a, b) -> RT.STRTree b
+createSTR things = runGeos $ do
+  tree <- RT.createSTRTree 10
+  foldM ins tree things
+  where ins tree' (g,b) = do
+          rg :: RG.Geom <- convertGeometryToRaw g
+          RT.insert tree' rg b
 
+querySTR :: Storable b => RT.STRTree b -> Geometry a -> V.Vector b
+querySTR tree g = runGeos $ do
+  rg :: RG.Geom <- convertGeometryToRaw g
+  RT.query tree rg
 
+-- query :: (Storable b, RG.Geometry a) => (STRTree b) -> a -> Geos (V.Vector b)
+--  RP.prepare r
