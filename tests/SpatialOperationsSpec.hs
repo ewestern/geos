d794b99d255c67e717cb79c8922be8e7ce358704
Jamie Cook <jamie.cook@veitchlister.com.au>
2017-06-28 23:46:26 +1000

    Holy shit I just got STRTree to work!!!

--- tests/SpatialOperationsSpec.hs
+++ tests/SpatialOperationsSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 
 module SpatialOperationsSpec where
 
@@ -9,6 +10,7 @@ import qualified Data.Vector as V
 import Data.Geometry.Geos.Types
 import Data.Geometry.Geos.Geometry
 import Data.Geometry.Geos.Topology
+import Data.Geometry.Geos.STRTree
 
 import SpecSampleData
 
@@ -64,3 +66,10 @@ spatialOpsSpecs = describe "Tests Contains" $ do
     (ensurePoint $ envelope point) `shouldBe` point
     (ensureMultiLineString $ boundary poly2) `shouldBe` env2
     convexHull poly2 `shouldBe` env3
+  it "can use STRTrees" $ do
+    let points = makePointGeo <$> [(0.1,0.1), (0.9, 0.9)]
+        polygon = makePolygonGeo [[(0,0),(0,1),(1,1),(1,0),(0,0)]]
+        foo = V.fromList $ zip points [(0::Int)..]
+    let tree = createSTR foo
+    let result = querySTR tree polygon
+    result `shouldBe` V.fromList [0,1]
