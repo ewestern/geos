818ed9ae245ca669e5363d73a83a8eb6ad60fe9e
Jamie Cook <jamie.cook@veitchlister.com.au>
2017-06-29 18:18:06 +1000

    wip

--- src/Data/Geometry/Geos/CSV.hs
+++ src/Data/Geometry/Geos/CSV.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Data.Geometry.Geos.CSV where
 
+import Data.Geometry.Geos.Types
+import Data.Geometry.Geos.Serialize
 import Data.Csv(FromField(parseField))
 
 instance FromField (Some Geometry) where
-  parseField s = pure $ readHex
+  parseField s = readHex <$> 
 
 -- instance FromJSON (Some Geometry)       where parseJSON = parseSomeGeometry
 -- instance A.FromJSON (Geometry Polygon)    where parseJSON = (fmap ensurePolygon)    . parseSomeGeometry
